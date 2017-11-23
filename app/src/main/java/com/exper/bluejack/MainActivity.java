d6c891dff293a1e9f7d19135841fe6f00a1f318d
mishaelm <mishaelm@walla.co.il>
2017-11-23 15:25:27 +0200

    connection  established --todo--  - check first connection slow response  - create settings dialog

--- app/src/main/java/com/exper/bluejack/MainActivity.java
+++ app/src/main/java/com/exper/bluejack/MainActivity.java
@@ -2,19 +2,28 @@ package com.exper.bluejack;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.os.StatFs;
+import android.graphics.Color;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.lang.ref.WeakReference;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,11 +31,40 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     private static String TAG = "mmmMainActivity";
+    private static boolean enableAttack = false;
+
+    private static Handler handler = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView list = findViewById(R.id.lisView);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                view.setBackgroundColor(Color.parseColor("#3F7AE0"));
+                enableAttack = true;
+            }
+        });
+
+        handler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message message) {
+                ImageButton btn =  findViewById(R.id.attack_indicator);
+                btn.setImageDrawable(getResources().getDrawable(R.drawable.btn_off3));
+
+                Log.v(TAG, "handler received message");
+                if (message.arg1 == 0){
+                    Bundle temp = message.getData();
+                    TextView textView = findViewById(R.id.server_response_text);
+                    textView.setText(temp.getString("server_response"));;
+                }
+                return false;
+            }
+        });
+
+
     }
 
 
@@ -41,52 +79,99 @@ public class MainActivity extends AppCompatActivity {
             s.add(bt.getName());
 
         list.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item_layout, s));
-
-
     }
+
+
 
     public void attackBtnClicked(View v){
-        new Thread(new run()).start();
+        if (enableAttack){
+            ImageButton attackIndicator =  findViewById(R.id.attack_indicator);
+            attackIndicator.setImageDrawable(getResources().getDrawable(R.drawable.btn_on3));
+            new Thread(new Run()).start();
+        }else{
+            Toast.makeText(this, "you must choose a device to attack", Toast.LENGTH_LONG).show();
+        }
+
 
     }
+
+
+
 
     public void resetBtnClicked(View v){
 
     }
 
-    class run implements Runnable{
+
+    public void settingsBtnClicked(View v){
+
+    }
+
+    private void setServerResponse(String response){
+        TextView textView =  findViewById(R.id.server_response_text);
+        textView.setText(response);
+    }
+
+
+    static class Run implements Runnable{
+        static boolean running = false;
         @Override
-        public void run() {
+        public synchronized void run() {
+            enableAttack = false;
             //Create socket connection
+            String serverResponse = null;
+            Message msg = new Message();
             try{
                 Log.v(TAG,"creating socket" );
                 Socket socket = new Socket("192.168.1.103", 4321);
+//                socket.setSoTimeout(10000);
+                if (socket.isConnected()){
+                    Log.v(TAG, "is connected");
+                }
+                if (socket.isBound()){
+                    Log.v(TAG, "is bound");
+                }
+                if (socket.isInputShutdown()){
+                    Log.v(TAG, "InputShutdown");
+                }
+
+                if (socket.isOutputShutdown()){
+                    Log.v(TAG, "OutputShutdown");
+                }
+
 
                 PrintWriter out = new PrintWriter(socket.getOutputStream(),    true);
                 BufferedReader in = new BufferedReader(new InputStreamReader( socket.getInputStream()));
                 Log.v(TAG,"sending data" );
-                out.println("hey there");
-
-
-
-
-
-
+                out.println("hallo there");
                 try{
-                    String line = in.readLine();
-                    Log.v(TAG,"Text received: " + line);
+                    serverResponse = in.readLine();
+                    Log.v(TAG,"Text received: " + serverResponse);
+                    if (serverResponse != null){
+                        msg.arg1 = 0;
+                    }
                 } catch (IOException e){
+                    msg.arg1 = -1;
                     Log.v(TAG,"Read failed");
-                    System.exit(1);
                 }
             } catch (UnknownHostException e) {
+                msg.arg1 = -1;
                 Log.v(TAG,"Unknown host: 192.168.1.103" );
                 System.out.println("Unknown host: kq6py");
-                System.exit(1);
             } catch  (IOException e) {
+                msg.arg1 = -1;
                 Log.v(TAG,"No I/O" );
-                System.exit(1);
             }
+            if (msg.arg1 == 0){
+                Bundle bundle = new Bundle();
+                bundle.putString("server_response", serverResponse);
+                msg.setData(bundle);
+            }
+
+            handler.sendMessage(msg);
+
+
+
         }
     }
 
---
+++ app/src/main/java/com/exper/bluejack/SettingsDialog.java
@@ -0,0 +1,15 @@
+package com.exper.bluejack;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by mishael on 11/23/17.
+ */
+
+public class SettingsDialog extends Dialog {
+    public SettingsDialog(@NonNull Context context) {
+        super(context);
+    }
+}
