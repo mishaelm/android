b6f94b5645165b60539195d5715ceb2061c47f45
mishaelm <mishaelm@walla.co.il>
2017-11-27 02:34:57 +0200

    connection  established --todo--  - check first connection slow response!!!  - did dialog

--- app/src/main/java/com/exper/bluejack/MainActivity.java
+++ app/src/main/java/com/exper/bluejack/MainActivity.java
@@ -1,14 +1,26 @@
 package com.exper.bluejack;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -21,32 +33,50 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.lang.ref.WeakReference;
 import java.net.Socket;
-import java.net.SocketAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-    private static String TAG = "mmmMainActivity";
+    private static final String TAG = "mmmMainActivity";
     private static boolean enableAttack = false;
 
+    public static String IP = "192.168.1.102";
+    public static int PORT = 4321;
+
     private static Handler handler = null;
+
+    private ListView listView;
+    private BluetoothAdapter mBluetoothAdapter;
+    private ArrayList<String> mDeviceList = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView list = findViewById(R.id.lisView);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listView = findViewById(R.id.lisView);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 view.setBackgroundColor(Color.parseColor("#3F7AE0"));
                 enableAttack = true;
             }
         });
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+//        IntentFilter filter = new IntentFilter();
+//        filter.addAction(BluetoothDevice.ACTION_FOUND);
+//        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+//        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+//
+//        registerReceiver(mReceiver, filter);
+
+
 
         handler = new Handler(new Handler.Callback() {
             @Override
@@ -59,7 +89,10 @@ public class MainActivity extends AppCompatActivity {
                     Bundle temp = message.getData();
                     TextView textView = findViewById(R.id.server_response_text);
                     textView.setText(temp.getString("server_response"));;
+                }else{
+                    Toast.makeText(getApplicationContext(), "An Error accuired, check your connection and try again", Toast.LENGTH_LONG).show();
                 }
+                unSelectListOptions();
                 return false;
             }
         });
@@ -67,18 +100,61 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(TAG, "onstart");
 
+
+
+
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.BLUETOOTH)){
+
+            }
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.BLUETOOTH}, 4251);
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_CALENDAR}, 4251);
+        }
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.WRITE_CALENDAR}, 4251);
+
+
+
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 4251){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Button button =  findViewById(R.id.scan_btn);
+                button.setEnabled(true);
+            }
+        }
+    }
 
     public void scanBtnClicked(View v){
+        if (!mBluetoothAdapter.isEnabled()){
+            Toast.makeText(this, "Enable bluetooth before this operation", Toast.LENGTH_LONG).show();
+        }
+        mBluetoothAdapter.startDiscovery();
         ListView list = findViewById(R.id.lisView);
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        mDeviceList = new ArrayList<String>();
+        for(BluetoothDevice bt : pairedDevices){
+            mDeviceList.add(bt.getName());
+        }
+        list.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item_layout, mDeviceList));
+    }
 
-        List<String> s = new ArrayList<String>();
-        for(BluetoothDevice bt : pairedDevices)
-            s.add(bt.getName());
-
-        list.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item_layout, s));
+    private void unSelectListOptions(){
+        listView.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item_layout, mDeviceList));
     }
 
 
@@ -99,11 +175,27 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void resetBtnClicked(View v){
-
+        TextView textView = findViewById(R.id.server_response_text);
+        textView.setText("");
+        IP = "192.168.1.102";
+        PORT = 4321;
+        mDeviceList = new ArrayList<>();
+        listView.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item_layout, mDeviceList));
     }
 
 
     public void settingsBtnClicked(View v){
+        SettingsDialog dialog = new SettingsDialog(this);
+        WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+
+        dialog.getWindow().setLayout(width - (width/4), height - (height / 2));
+
+        dialog.show();
 
     }
 
@@ -123,7 +215,7 @@ public class MainActivity extends AppCompatActivity {
             Message msg = new Message();
             try{
                 Log.v(TAG,"creating socket" );
-                Socket socket = new Socket("192.168.1.103", 4321);
+                Socket socket = new Socket(IP, PORT);
 //                socket.setSoTimeout(10000);
                 if (socket.isConnected()){
                     Log.v(TAG, "is connected");
@@ -141,9 +233,10 @@ public class MainActivity extends AppCompatActivity {
 
 
                 PrintWriter out = new PrintWriter(socket.getOutputStream(),    true);
-                BufferedReader in = new BufferedReader(new InputStreamReader( socket.getInputStream()));
+
                 Log.v(TAG,"sending data" );
-                out.println("hallo there");
+                out.println("hallo there bla bla\r\n");
+                BufferedReader in = new BufferedReader(new InputStreamReader( socket.getInputStream()));
                 try{
                     serverResponse = in.readLine();
                     Log.v(TAG,"Text received: " + serverResponse);
@@ -175,4 +268,41 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+
+
+    @Override
+    protected void onDestroy() {
+
+//        if (mReceiver != null){
+//            unregisterReceiver(mReceiver);
+//        }
+        super.onDestroy();
+    }
+
+
+
+
+//    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//
+//            final String action = intent.getAction();
+//
+//            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+//                Log.v(TAG, "starting discovery");
+//                //discovery starts, we can show progress dialog or perform other tasks
+//            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+//                Log.v(TAG, "finished discovery");
+//                //discovery finishes, dismis progress dialog
+//            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+//                //bluetooth device found
+//                Log.v(TAG, "found bluetooth device");
+//                Log.v(TAG, "found bluetooth device");
+//                Log.v(TAG, "found bluetooth device");
+//                Log.v(TAG, "found bluetooth device");
+//
+//            }
+//        }
+//    };
 }
--- app/src/main/java/com/exper/bluejack/SettingsDialog.java
+++ app/src/main/java/com/exper/bluejack/SettingsDialog.java
@@ -3,13 +3,36 @@ package com.exper.bluejack;
 import android.app.Dialog;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by mishael on 11/23/17.
  */
 
 public class SettingsDialog extends Dialog {
+    EditText ipText;
+    EditText portText;
+    Button submitBtn;
+
+
     public SettingsDialog(@NonNull Context context) {
         super(context);
+        setContentView(R.layout.dialog_layout);
+        ipText = findViewById(R.id.dialog_ip_text);
+        portText = findViewById(R.id.dialog_port_text);
+        submitBtn = findViewById(R.id.dialog_submit_btn);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.IP = ipText.getText().toString();
+                MainActivity.PORT = Integer.valueOf(portText.getText().toString());
+                dismiss();
+            }
+        });
+
+
+
     }
 }
